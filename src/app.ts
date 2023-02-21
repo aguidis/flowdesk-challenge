753bf054393dad95b28b87b507c405c3177c8b16
aguidis <adrien.guidis@outlook.com>
2023-02-21 11:22:27 +0100

    update route controller

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,22 @@ import { getPriceIndex as fetchKrakenPriceIndex } from '~/api/kraken'
 import { getPriceIndex as fetchBinancePriceIndex } from '~/api/binance'
 import { getPriceIndex as fetchHuobiPriceIndex } from '~/api/huobi'
 import { computeAverage } from '~/utils/average'
-import { isFulfilled, isRejected } from '~/types/PromiseSettledState'
+import { isFulfilled, isRejected } from '~/helper/promise'
 
 const app = express()
 
 app.get('/order-book/:pair/global-price-index', (async (_request: Request, response: Response) => {
     const pair = _request.params.pair
 
+    const [prices, rejectedReasons] = await getGlobalPriceIndex(pair)
+
+    response.send({
+        indexPrice: computeAverage(prices),
+        errors: rejectedReasons,
+    })
+}) as RequestHandler)
+
+async function getGlobalPriceIndex(pair): Promise<[number[], string[]]> {
     const krakenPriceIndex = fetchKrakenPriceIndex(pair)
     const binancePriceIndex = fetchBinancePriceIndex(pair)
     const huobiPriceIndex = fetchHuobiPriceIndex(pair)
@@ -23,10 +32,7 @@ app.get('/order-book/:pair/global-price-index', (async (_request: Request, respo
 
     const rejectedReasons = results.filter(isRejected).map(p => p.reason)
 
-    response.send({
-        indexPrice: computeAverage(prices),
-        errors: rejectedReasons,
-    })
-}) as RequestHandler)
+    return [prices, rejectedReasons]
+}
 
 export default app
